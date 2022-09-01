fe6bd9af185b843625b2fcc3cf00681247e86b69
Eleanor Ruth <ellie.ruth@me.com>
2022-09-01 08:41:48 -0600

    Complete magician class using TDD

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -3,6 +3,25 @@ class Magician {
     this.name = "The Great " + magicianObject.name
     this.assistant = magicianObject.assistant
     this.favoriteAccessory = magicianObject.clothing || 'top hat'
+    this.confidencePercentage = 10
+  }
+  performIncantation(magicianWords) {
+    return magicianWords.toUpperCase() + "!"
+  }
+  performTrick() {
+    this.confidencePercentage +=10
+    if (this.favoriteAccessory === 'top hat') {
+      return 'PULL RABBIT FROM TOP HAT'
+    } else {
+      return 'PULL DOVE FROM SLEEVE'
+    }
+  }
+  performShowStopper() {
+    if (this.confidencePercentage < 100 || !this.assistant) {
+      return 'Oh no, this trick is not ready!'
+    } else {
+      return 'WOW!The magician totally just sawed that person in half!'
+    }
   }
 }
 
