48729d7e5270683957936e893d88c639f5f407ad
Eleanor Ruth <ellie.ruth@me.com>
2022-09-06 19:53:18 -0600

    Add Class

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,28 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(personInfo) {
+    //console.log(`personInfo.name: ${personInfo.name}`)
+    var newPerson = new Statue(personInfo.name)
+    if (this.statues.length >= 3) {
+      var becomeAPerson = new Person(this.statues[0].name)
+      becomeAPerson.mood = 'relieved'
+      this.statues.shift()
+      this.statues.push(newPerson)
+      return becomeAPerson
+    } else {
+      this.statues.push(newPerson)
+    }
+      //  console.log(new Person(this.statues[0].name))
+
+// TODO: refractor this if statement to end on an else.
+      //console.log(this.statues)
+    }
+  }
+
+module.exports = Medusa
