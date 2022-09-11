c92397561fa2c60f5978041a2b4118d1eb0329a0
Eleanor Ruth <ellie.ruth@me.com>
2022-09-11 17:31:04 -0600

    Complete stark class

--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,10 +1,23 @@
+var Direwolf = require('./direwolf');
+
 class Stark {
   constructor(starkObject) {
       this.name = starkObject.name
       this.location = starkObject.area || 'Winterfell'
+      this.safe = false
+    }
+    sayHouseWords() {
+      if (!this.safe) {
+        return 'Winter is Coming'
+      } else {
+        return 'The North Remembers'
+      }
+    }
+    callDirewolf(name, location) {
+      var newDirewolf = new Direwolf(name, this.location)
+        newDirewolf.protect(this)
+        return newDirewolf
     }
   }
-
-console.log(`STARK!!! ${Stark.location}`)
 
 module.exports = Stark
