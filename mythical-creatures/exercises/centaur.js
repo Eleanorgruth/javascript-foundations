4646d8b4312c6eaee16202b948817bbb06871c52
Eleanor Ruth <ellie.ruth@me.com>
2022-09-11 17:30:44 -0600

    Complete centaur class

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -1,5 +1,55 @@
 class Centaur {
+  constructor(centaurInfo) {
+    this.name = centaurInfo.name;
+    this.breed = centaurInfo.type;
+    this.cranky = false;
+    this.standing = true;
+    this.counter = 0;
+    this.layingDown = false;
+  }
 
+shootBow() {
+  this.counter += 1
+  if (this.cranky) {
+    return 'NO!'
+  } else if (this.counter >= 3) {
+    this.cranky = true
+    } return 'Twang!!!'
+  }
+
+run() {
+  this.counter += 1
+if (this.cranky) {
+    return 'NO!'
+  } else if (this.counter >= 3) {
+  this.cranky = true}
+    return 'Clop clop clop clop!!!'
+}
+sleep() {
+ if (this.standing) {
+   return 'NO!'
+ } else if (this.layingDown) {
+   this.cranky = false
+   return 'ZZZZ'
+ }
+}
+
+layDown() {
+  this.standing = false
+  this.layingDown = true
+  this.cranky = true
+}
+standUp() {
+  this.standing = true
+  this.layingDown = false
+}
+drinkPotion() {
+  if (!this.standing) {
+    return 'Not while I\'m laying down!'
+  } else {
+    this.cranky = false
+  }
+}
 }
 
 
