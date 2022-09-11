c80fc1e504ba81c5c9d0306931507de7a851df19
Eleanor Ruth <ellie.ruth@me.com>
2022-09-11 17:27:28 -0600

    Complete Lift class

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,32 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(liftLimit) {
+    this.inService = true;
+    this.limit = liftLimit;
+    this.skiers = [];
+    this.safetyBar = 'up'
+  }
+  admitSkier(name, liftTicket) {
+    var skier = new Skier(name, liftTicket)
+      if(this.skiers.length < this.limit && skier.hasLiftTicket) {
+      this.skiers.push(skier)
+    } else if (this.skiers.length >= this.limit) {
+      return `Sorry, ${skier.name}. Please wait for the next lift!`
+    } else if (!skier.hasLiftTicket) {
+      return `Sorry, ${skier.name}. You need a lift ticket!`
+    }
+  }
+  startLift() {
+    var skierAmount = this.limit - this.skiers.length
+    if(skierAmount === 0) {
+      this.safetyBar = 'down'
+    } else if (skierAmount === 1) {
+      return `We still need ${skierAmount} more skier!`
+    } else {
+      return `We still need ${skierAmount} more skiers!`
+    }
+  }
 }
 
 module.exports = Lift;
